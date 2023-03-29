cd0921c9f3fa5b4154c56137953f0c8f7f4bf662
ilyakoren <IlyaKoren@coherentsolutions.com>
2023-03-29 16:20:12 +0300

    added new gitignore values, changed config, added requirments

--- config.py
+++ config.py
@@ -4,5 +4,4 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://<username>:<password>@localhost/<database>'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
